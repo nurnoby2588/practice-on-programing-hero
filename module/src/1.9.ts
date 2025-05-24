05a3b9318695b23d80f1ac842eb6d608f97af051
nurnoby admin <nurnoby323@gmail.com>
2025-05-24 09:34:57 +0600

    Learn object type alias

---
+++ module/src/1.9.ts
@@ -0,0 +1,29 @@
+// type alias
+{
+// object type alias
+
+type Student={
+    name: string;
+    age:number;
+    contact?:string;
+    address:string
+}
+    
+
+
+const student1 : Student={
+    name:"nurnoby",
+    age:26,
+    contact:"01710258850",
+    address:"Tangail"
+}
+const student2 : Student={
+    name:"Nasim",
+    age:27,
+    address:"Sirajgonj"
+}
+
+
+
+
+}
